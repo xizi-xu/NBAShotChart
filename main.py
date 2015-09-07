da79d7909f5599f7e588fc6dca06ccfe75de17aa
Liuxizi Xu <xlxzbisulyn@gmail.com>
2015-09-07 14:50:59 -0400

    fetch data and display shots

---
+++ main.py
@@ -0,0 +1,29 @@
+# %matplotlib inline
+import requests
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+
+shot_chart_url = 'http://stats.nba.com/stats/shotchartdetail?CFID=33&CFPAR'\
+                'AMS=2014-15&ContextFilter=&ContextMeasure=FGA&DateFrom=&D'\
+                'ateTo=&GameID=&GameSegment=&LastNGames=0&LeagueID=00&Loca'\
+                'tion=&MeasureType=Base&Month=0&OpponentTeamID=0&Outcome=&'\
+                'PaceAdjust=N&PerMode=PerGame&Period=0&PlayerID=203076&Plu'\
+                'sMinus=N&Position=&Rank=N&RookieYear=&Season=2014-15&Seas'\
+                'onSegment=&SeasonType=Regular+Season&TeamID=0&VsConferenc'\
+                'e=&VsDivision=&mode=Advanced&showDetails=0&showShots=1&sh'\
+                'owZones=0'
+
+# Get the webpage containing the data
+response = requests.get(shot_chart_url)
+# Grab the headers to be used as column headers for our DataFrame
+headers = response.json()['resultSets'][0]['headers']
+# Grab the shot chart data
+shots = response.json()['resultSets'][0]['rowSet']
+
+shot_df = pd.DataFrame(shots, columns=headers)
+sns.set_style("white")
+sns.set_color_codes()
+plt.figure(figsize=(12,11))
+plt.scatter(shot_df.LOC_X, shot_df.LOC_Y)
+plt.show()
